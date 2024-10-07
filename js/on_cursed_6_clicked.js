59e6f696164de1f58b384cbcce29fcd79dfb8455
zxcpisya <mamin.papa0@gmail.com>
2024-10-07 22:19:25 +1000

    update all files to current state

--- js/on_cursed_6_clicked.js
+++ js/on_cursed_6_clicked.js
@@ -1,5 +1,5 @@
 let button_open_links = document.querySelector(".image_container_3")
-let link_ = 'info.html'
+let link_ = 'https://t.me/ad_bana'
 
 button_open_links.addEventListener("click", () => {
     window.open(link_, '_blank').focus()
