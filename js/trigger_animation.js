3cc799bbd00ecc5fc411ffc99788248736c355df
zxcpisya <mamin.papa0@gmail.com>
2024-10-07 23:07:54 +1000

    removed logs string

--- js/trigger_animation.js
+++ js/trigger_animation.js
@@ -2,11 +2,7 @@ let intro_container = document.querySelector(".intro")
 
 let doc = document.querySelector(":root")
 
-console.log(doc)
-console.log(intro_container)
-
 doc.addEventListener("keydown", (event) => {
-    console.log(event, event.code)
     if (event.code === 'Space') {
         intro_container.classList.add("anim")
     }
