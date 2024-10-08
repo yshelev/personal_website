f15a3ae5cd1a418be4208400678a0261a9dbc5ea
zxcpisya <mamin.papa0@gmail.com>
2024-10-09 02:50:29 +1000

    renamed css class anim -> anim__space

--- js/trigger_animation.js
+++ js/trigger_animation.js
@@ -4,10 +4,10 @@ let doc = document.querySelector(":root")
 
 doc.addEventListener("keydown", (event) => {
     if (event.code === 'Space') {
-        intro_container.classList.add("anim")
+        intro_container.classList.add("anim__space")
     }
 
     setTimeout(() => {
-        intro_container.classList.remove("anim")
+        intro_container.classList.remove("anim__space")
     }, 100)
 })
