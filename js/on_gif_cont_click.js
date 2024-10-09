1e29fef14abe83dac677d206842d0866c2357345
zxcpisya <mamin.papa0@gmail.com>
2024-10-09 11:35:22 +1000

    on cursed play fadeout

--- js/on_gif_cont_click.js
+++ js/on_gif_cont_click.js
@@ -1,6 +1,7 @@
 const AUDIO_PATH = "../audio/"
 
 let button_play = document.querySelector(".gif_container")
+let cursed_container = document.querySelector(".container_cursed")
 let is_playing = false;
 
 let current_audio_index = 0;
@@ -11,9 +12,26 @@ let audios = [
     new Audio(AUDIO_PATH + "cupsize_love_me_alina.mp3")
 ]
 
+audios[0].loop = false
+
 audios.forEach((audio) => audio.volume = .1)
 
 button_play.addEventListener("click", () => {
+    if (current_audio_index === 0 && audios[current_audio_index].currentTime < 0.001) {
+        intro_container.classList.add("anim__cursed")
+
+        setTimeout(() => {
+            cursed_container.classList.add("anim__show")
+        }, 10000)
+
+        setTimeout(() => {
+            intro_container.classList.remove("anim__cursed")
+            cursed_container.classList.remove("anim__show")
+        }, 33000)
+    }
+
+
+
     if (is_playing) {
         audios[current_audio_index].pause()
         is_playing = false
