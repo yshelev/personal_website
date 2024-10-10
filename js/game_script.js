fdde9a12b70eb925925bebc3227c2d67c16e190c
zxcpisya <mamin.papa0@gmail.com>
2024-10-10 22:34:55 +1000

    fadeout effect on icons when they clicked

--- js/game_script.js
+++ js/game_script.js
@@ -16,6 +16,8 @@ let links = [
     "https://youtu.be/ZPcPHLFFsds?si=47-DK1HUVYxgRtAX"
 ]
 
+const icons = coils_container.getElementsByClassName('coil_icon_container')
+
 let zxc_permutations = [
     "KeyZ,KeyX,KeyC",
     "KeyZ,KeyC,KeyX",
@@ -38,6 +40,10 @@ let interval_start = null
 
 let hidden_link = "https://mesenev.ru/"
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 function endMiniGame() {
     coils_container.classList.remove("on_game_started")
     window.open(links[Math.min(streak - 1, zxc_permutations.length - 1)], "blank_")
@@ -53,27 +59,36 @@ function endMiniGame() {
 
 doc.addEventListener("keydown", (event) => {
     keys.push(event.code)
+    if (coils.indexOf(event.code.toString()) >= 0) {
+        icons[coils.indexOf(event.code.toString())].classList.add("clicked_animation")
+    }
     if (keys.toString().indexOf(zxc_permutations[index_of_current_zxc_string]) >= 0 ) {
         if (is_playing_in_minigame) {
-            if ((Date.now() - interval_start) / 1000 >= 10) {
+            if ((Date.now() - interval_start) / 1000 >= 4) {
                 endMiniGame()
             }
         }
 
-        interval_start = Date.now()
-        streak += 1
-        if (streak === 1) {
-            coils_container.classList.add("on_game_started")
-            is_playing_in_minigame = true
-        }
-        index_of_current_zxc_string = getRandomInt(zxc_permutations.length)
-        coils = zxc_permutations[index_of_current_zxc_string].split(",")
+        sleep(400).then(() => {
+            interval_start = Date.now()
+            streak += 1
+            if (streak === 1) {
+                coils_container.classList.add("on_game_started")
+                is_playing_in_minigame = true
+            }
+            index_of_current_zxc_string = getRandomInt(zxc_permutations.length)
+            coils = zxc_permutations[index_of_current_zxc_string].split(",")
 
-        coils_container.innerHTML = `
-        <img class="coil_icon_container" src=${dict_button_resource[coils[0]]} alt=""/>
-        <img class="coil_icon_container" src=${dict_button_resource[coils[1]]} alt=""/>
-        <img class="coil_icon_container" src=${dict_button_resource[coils[2]]} alt=""/>`
-        keys = []
+            coils_container.innerHTML =
+                `
+                <img class="coil_icon_container" src=${dict_button_resource[coils[0]]} alt=""/>
+                <img class="coil_icon_container" src=${dict_button_resource[coils[1]]} alt=""/>
+                <img class="coil_icon_container" src=${dict_button_resource[coils[2]]} alt=""/>
+                `
+            keys = []
+        })
+
+
 
     } else if (keys.length >= 3 && is_playing_in_minigame) {
         endMiniGame()
